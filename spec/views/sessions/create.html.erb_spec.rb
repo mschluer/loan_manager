e4d926ec6a5eced40a5be7d0a353818c8691d522
Max Schluer <max.schluer@gmail.com>
2020-05-21 13:26:24 +0200

    test new session view

--- spec/views/sessions/create.html.erb_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "sessions/create.html.erb", type: :view do
-end
--- spec/views/sessions/destroy.html.erb_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "sessions/destroy.html.erb", type: :view do
-end
