5181667b3cb2576960cf96cad145ad4540af4090
Max Schluer <max.schluer@gmail.com>
2020-06-02 15:51:29 +0200

    show string on dashboard if there are overdues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -39,6 +39,8 @@ class HomeController < ApplicationController
     @paid_loans_amount = @loans.count - @active_loans_amount
 
     @most_recent_payments = Payment.where(loan_id: @loans).limit(10).order(id: :desc)
+
     @upcoming_payments = ScheduledPayment.where(loan_id: @loans).limit(10).order(date: :asc)
+    @overdue_upcoming_payments = @upcoming_payments.select { |element| element.overdue? }
   end
 end
